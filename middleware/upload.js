3de2c552afc5714a8ce7af8886d0b48322073f20
dung14007 <trungdung140703@gmail.com>
2026-07-10 13:21:57 +0700

    First commit

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,15 +1,36 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+
+const uploadDir = path.join(__dirname, "../public/uploads");
+
+
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, {
+        recursive: true
+    });
+}
+
 
 const storage = multer.diskStorage({
+
     destination(req, file, cb) {
-        cb(null, "public/uploads");
+
+        cb(null, uploadDir);
+
     },
+
     filename(req, file, cb) {
+
         const ext = path.extname(file.originalname);
+
         cb(null, Date.now() + ext);
+
     }
+
 });
+
 
 module.exports = multer({
     storage
