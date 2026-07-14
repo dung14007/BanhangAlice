67ed90f02a2d5df5e140736ebcd96bc8a1ac38bc
dung14007 <trungdung140703@gmail.com>
2026-07-14 16:37:24 +0700

    First commit

--- public/admin-sw.js
+++ public/admin-sw.js
@@ -1,3 +1,9 @@
+self.addEventListener("install", () => self.skipWaiting());
+
+self.addEventListener("activate", event => {
+    event.waitUntil(clients.claim());
+});
+
 self.addEventListener("notificationclick", event => {
     event.notification.close();
     const targetUrl = event.notification.data?.url || "/admin.html";
